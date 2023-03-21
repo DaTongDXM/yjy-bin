bcf07a41385107ff2c49cb4368a0ee4737658dfc
DaTongDXM <953909305@qq.com>
2023-03-21 20:53:54 +0800

    ✨ feat: 初始化init函数

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const param = process.argv[2] || 'start';
 
+const basePath = __dirname + '/test'
+
 const command = require(`./lib/${param}.js`)
 if (command) {
-  command()
+  command(basePath)
 }
